a8e1057e3314d66445ccde794cc8ebf5f383331a
Shayan <shayan.yazdanmehr@gmail.com>
2018-09-17 19:31:49 +0200

    Get the boundary value for multipication table from argv

--- loop.py
+++ loop.py
@@ -1,4 +1,13 @@
-for i in range(1,11):
-    for j in range(1,11):
+from sys import argv
+
+x = 11
+try:
+    if(argv[1].isdigit()):
+        x = int(argv[1]) + 1
+except Exception:
+    print("10x10 Multipication table")
+    
+for i in range(1,x):
+    for j in range(1,x):
         print("%3d" % (i*j)," ", end="")
     print("\n")
